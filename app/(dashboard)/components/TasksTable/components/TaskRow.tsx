20358092400ed3e54daf69617700b8ce97c7195c
junKitClover <jkchan.clovers@gmail.com>
2025-03-22 13:56:40 +0800

    [CB-0017]: Delete tasks

--- app/(dashboard)/components/TasksTable/components/TaskRow.tsx
+++ app/(dashboard)/components/TasksTable/components/TaskRow.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
@@ -9,15 +11,23 @@ import {
 import { MoreHorizontal } from 'lucide-react';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { SelectTasks } from '@/lib/db';
-import { deleteTasks } from '../../../actions';
 import { useTaskFormStore } from '@/store/TaskForm';
+import toast from 'react-hot-toast';
 
 export default function TaskRow({ task }: { task: SelectTasks }) {
   const { openForm } = useTaskFormStore();
   const editHandler = () => {
     openForm(task);
   };
-  const { name, description, due_date, status,created_on } = task;
+  const deleteHandler = async() => {
+    try{
+      await fetch(`/api/tasks/${task.id}`, { method: 'delete' });
+      toast('Delete Success');
+    } catch{
+      toast('Failed Delete');
+    }
+  };
+  const { name, description, due_date, status, created_on } = task;
 
   return (
     <TableRow>
@@ -45,7 +55,7 @@ export default function TaskRow({ task }: { task: SelectTasks }) {
           <DropdownMenuContent align="end">
             <DropdownMenuItem onClick={editHandler}>Edit</DropdownMenuItem>
             <DropdownMenuItem>
-              <form action={deleteTasks}>
+              <form action={deleteHandler}>
                 <button type="submit">Delete</button>
               </form>
             </DropdownMenuItem>
